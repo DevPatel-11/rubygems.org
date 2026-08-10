5b09a0d4235fa4e63701f9e1e5254e8acf78261c
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-08-11 01:53:02 +0530

    refactor: remove redundant canonical payload contract and refactor recorder.
    Signed-off-by: Amisha Chhajed <amishhhaaaa@gmail.com>

--- app/controllers/concerns/recorder.rb
+++ app/controllers/concerns/recorder.rb
@@ -6,53 +6,70 @@ module Recorder
   private
 
   def record_transparency_log_event(event_type, record)
-    attributes =
-      case record
-      when Pusher, Deletion
-        gem_version_attributes(event_type, record)
-      when User
-        ownership_change_attributes(event_type, record)
-      else
-        return
-      end
+    attributes = {
+      event_type: event_type,
+      authentication_method: "api_key",
+      **resource_attributes(record),
+      **subject_attributes(record),
+      **actor_attributes(record)
+    }
 
     TransparencyLog::Recorder.new.record(attributes)
   rescue StandardError => e
     Rails.error.report(e, handled: true)
   end
 
-  def gem_version_attributes(event_type, record)
-    version = record.version
-    rubygem = record.is_a?(Pusher) ? record.rubygem : version.rubygem
+  def resource_attributes(record)
+    rubygem =
+      case record
+      when Pusher
+        record.rubygem
+      when Deletion
+        record.version.rubygem
+      when User
+        @rubygem
+      end
 
     {
-      event_type: event_type,
       resource_type: "rubygem",
       resource_name: rubygem.name,
-      resource_id: rubygem.id.to_s,
-      subject_type: "gem_version",
-      subject_name: version.full_name,
-      subject_id: version.id.to_s,
-      actor_type: @api_key.user? ? "user" : "unknown",
-      actor_id: @api_key.owner_id.to_s,
-      actor_handle: @api_key.owner&.name,
-      authentication_method: "api_key"
+      resource_id: rubygem.id.to_s
     }
   end
 
-  def ownership_change_attributes(event_type, owner)
-    {
-      event_type: event_type,
-      resource_type: "rubygem",
-      resource_name: @rubygem.name,
-      resource_id: @rubygem.id.to_s,
-      subject_type: "user",
-      subject_name: owner.handle,
-      subject_id: owner.id.to_s,
-      actor_type: "user",
-      actor_id: @api_key.user.id.to_s,
-      actor_handle: @api_key.user.handle,
-      authentication_method: "api_key"
-    }
+  def subject_attributes(record)
+    case record
+    when Pusher, Deletion
+      version = record.version
+
+      {
+        subject_type: "gem_version",
+        subject_name: version.full_name,
+        subject_id: version.id.to_s
+      }
+    when User
+      {
+        subject_type: "user",
+        subject_name: record.handle,
+        subject_id: record.id.to_s
+      }
+    end
+  end
+
+  def actor_attributes(record)
+    case record
+    when Pusher, Deletion
+      {
+        actor_type: @api_key.user? ? "user" : "unknown",
+        actor_id: @api_key.owner_id.to_s,
+        actor_handle: @api_key.owner&.name
+      }
+    when User
+      {
+        actor_type: "user",
+        actor_id: @api_key.user.id.to_s,
+        actor_handle: @api_key.user.handle
+      }
+    end
   end
 end
--- test/models/concerns/recorder_test.rb
+++ test/models/concerns/recorder_test.rb
@@ -13,9 +13,9 @@ class RecorderTest < ActiveSupport::TestCase
     user = create(:user)
     api_key = create(:api_key, owner: user)
     rubygem = create(:rubygem, name: "test")
-    version = create(:version, rubygem: rubygem)
-
+    version = create(:version, rubygem:)
     pusher = Pusher.allocate
+
     pusher.stubs(:rubygem).returns(rubygem)
     pusher.stubs(:version).returns(version)
 
@@ -23,7 +23,6 @@ class RecorderTest < ActiveSupport::TestCase
 
     recorder = mock
     TransparencyLog::Recorder.expects(:new).returns(recorder)
-
     recorder.expects(:record).with({
       event_type: "gem_push",
       resource_type: "rubygem",
@@ -38,7 +37,37 @@ class RecorderTest < ActiveSupport::TestCase
       authentication_method: "api_key"
     })
 
-    @controller.send(:record_transparency_log_event, "gem_push", pusher)
+    assert_nothing_raised { @controller.send(:record_transparency_log_event, "gem_push", pusher) }
+  end
+
+  test "records a gem deletion event when record is a Deletion" do
+    user = create(:user)
+    api_key = create(:api_key, owner: user)
+    rubygem = create(:rubygem)
+    version = create(:version, rubygem:)
+    deletion = Deletion.allocate
+
+    Deletion.any_instance.stubs(:version).returns(version)
+
+    @controller.instance_variable_set(:@api_key, api_key)
+
+    recorder = mock
+    TransparencyLog::Recorder.expects(:new).returns(recorder)
+    recorder.expects(:record).with({
+      event_type: "gem_yank",
+      resource_type: "rubygem",
+      resource_name: rubygem.name,
+      resource_id: rubygem.id.to_s,
+      subject_type: "gem_version",
+      subject_name: version.full_name,
+      subject_id: version.id.to_s,
+      actor_type: api_key.user? ? "user" : "unknown",
+      actor_id: api_key.owner_id.to_s,
+      actor_handle: api_key.owner&.name,
+      authentication_method: "api_key"
+    })
+
+    assert_nothing_raised { @controller.send(:record_transparency_log_event, "gem_yank", deletion) }
   end
 
   test "records an ownership change event when record is a User" do
@@ -52,7 +81,6 @@ class RecorderTest < ActiveSupport::TestCase
 
     recorder = mock
     TransparencyLog::Recorder.expects(:new).returns(recorder)
-
     recorder.expects(:record).with({
       event_type: "owner_add",
       resource_type: "rubygem",
@@ -67,35 +95,30 @@ class RecorderTest < ActiveSupport::TestCase
       authentication_method: "api_key"
     })
 
-    @controller.send(:record_transparency_log_event, "owner_add", owner)
+    assert_nothing_raised { @controller.send(:record_transparency_log_event, "owner_add", owner) }
   end
 
   test "does not record an event for an unsupported record type" do
     TransparencyLog::Recorder.expects(:new).never
 
-    @controller.send(:record_transparency_log_event, "unknown_event", Object.new)
+    assert_nothing_raised { @controller.send(:record_transparency_log_event, "unknown_event", Object.new) }
   end
 
   test "reports errors without raising them" do
     api_key = create(:api_key)
     rubygem = create(:rubygem)
-    version = create(:version, rubygem: rubygem)
+    version = create(:version, rubygem:)
+    pusher = Pusher.allocate
     error = StandardError.new("recording failed")
 
-    pusher = Pusher.allocate
     pusher.stubs(:rubygem).returns(rubygem)
     pusher.stubs(:version).returns(version)
 
     @controller.instance_variable_set(:@api_key, api_key)
 
-    TransparencyLog::Recorder
-      .expects(:new)
-      .raises(error)
-
+    TransparencyLog::Recorder.expects(:new).raises(error)
     Rails.error.expects(:report).with(error, handled: true)
 
-    assert_nothing_raised do
-      @controller.send(:record_transparency_log_event, "gem_push", pusher)
-    end
+    assert_nothing_raised { @controller.send(:record_transparency_log_event, "gem_push", pusher) }
   end
 end
